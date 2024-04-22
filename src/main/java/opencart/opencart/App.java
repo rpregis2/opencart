69a12683baafa366b7430bf13356d0112587e861
Rosario Sigamani <rpregis2@gmail.com>
2024-04-22 15:36:04 -0400

    added demo page and changes app.java

--- src/main/java/opencart/opencart/App.java
+++ src/main/java/opencart/opencart/App.java
@@ -4,8 +4,9 @@ package opencart.opencart;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+private int a=10;
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
---
+++ src/main/java/opencart/opencart/DemoPage.java
@@ -0,0 +1,10 @@
+package opencart.opencart;
+
+public class DemoPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=10;
+	}
+
+}
